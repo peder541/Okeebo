778e820d06450a695d4dec8f136ddbc63542a664
Ben Pedersen <peder541@umn.edu>
2013-11-21 18:40:17 -0600

    app

--- www/js/app.js
+++ www/js/app.js
@@ -141,11 +141,12 @@ $(document).on('ready',function(event) {
 		}
 		else if ($url == "javascript:navigator.id.request()") {
 			var persona = window.open('https://login.persona.org/sign_in#NATIVE','_self','location=yes');
-			persona.addEventListener('loadStart',function(event) {
+			/*persona.addEventListener('loadStart',function(event) {
 				BrowserID.internal.get('https://www.okeebo.com', function(assertion) { 
 					persona.close(); 
 				});
-			});
+			});*/
+			persona.close();
 			return false;
 			$.get('https://login.persona.org/sign_in#NATIVE',function(data) {
 				parent.$('html').html(data);
