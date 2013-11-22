f019157ef3f9478d4c88c84a497da1e47658d0aa
Ben Pedersen <peder541@umn.edu>
2013-11-21 23:35:21 -0600

    app

--- www/js/persona_bridge.js
+++ www/js/persona_bridge.js
@@ -1,14 +1,15 @@
 // JavaScript Document
 
-var event;
-if (document.createEvent) {
-	event = document.createEvent('HTMLEvents');
-	event.initEvent('relay', true, true);
-}
-else {
-	event = document.createEventObject();
-	event.eventType = 'relay';
-}
 BrowserID.internal.get('https://www.okeebo.com', function(assertion) {
+	var event;
+	if (document.createEvent) {
+		event = document.createEvent('HTMLEvents');
+		event.initEvent('relay', true, true);
+	}
+	else {
+		event = document.createEventObject();
+		event.eventType = 'relay';
+	}
+	event.data = assertion;
 	document.dispatchEvent(event);
 })
