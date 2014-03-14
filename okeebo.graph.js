87391ffc285208c41cce7e9ae1e2d20732b4ec28
Ben Pedersen <peder541@umn.edu>
2014-03-14 06:37:17 -0500

    Update okeebo.graph.js

--- okeebo.graph.js
+++ okeebo.graph.js
@@ -50,13 +50,7 @@ $(document).ready(function(event) {
 						if (page_id == '`1') page_id = 'Z1';
 						var summary_id = String.fromCharCode(page_id.charCodeAt(0)-1) + page_id.substr(1);
 						toggle_graph();
-						if (event.ctrlKey) {	// Clone Page rather than "Multiple Inheritance"
-							var $clone = $('.' + page_id).clone().attr('id','');
-							if ($clone.hasClass('outer')) $clone.attr('class','outer inner');
-							else $clone.attr('class','inner');
-							insert_page($('#' + summary_id).clone(),$clone);
-						}
-						else insert_page($('#' + summary_id).clone(),$('.' + page_id),1);
+						insert_page($('#' + summary_id).clone(),$('.' + page_id),1);
 						if (continuous) toggle_graph(1);
 					}
 				}
